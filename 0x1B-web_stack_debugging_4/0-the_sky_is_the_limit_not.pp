eb08b6ba44812724947f906a369f4b4ea52d473a
Pharel Makona <pharel@Pharel.localdomain>
2024-06-22 11:48:41 +0300

    deferment

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,3 +0,0 @@
-# Changes nginx ULIMIT to 1024
-exec { '/usr/bin/env sed -i s/15/1024/ /etc/default/nginx': }
--> exec { '/usr/bin/env service nginx restart': }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,2 +0,0 @@
-# Change the OS configuration to enable login with the holberton user and open a file without any error message.
-exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
